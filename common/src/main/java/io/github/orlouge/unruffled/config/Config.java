a1793186ea84d922d94a11719be66cfcaa90709d
orlouge <orlouge@protonmail.com>
2025-02-08 15:54:02 +0100

    Added item frame, chiseled bookshelf and decorated pot enchantment filtering

--- common/src/main/java/io/github/orlouge/unruffled/config/Config.java
+++ common/src/main/java/io/github/orlouge/unruffled/config/Config.java
@@ -142,11 +142,14 @@ public class Config {
 
     public record EnchantmentsConfig(
         boolean disableEnchantingTable, boolean disableGrindstone, boolean disableRepairXpCost, boolean disableGlint, boolean showLevelNumber,
-        Optional<Boolean> disenchantChiseledBookshelfBooks,
+        Optional<Boolean> disenchantChiseledBookshelfBooks, Optional<Boolean> filterStructureItemFrames, Optional<Boolean> filterStructureChiseledBookshelves, Optional<Boolean> filterStructureDecoratedPots,
         Set<RegistryKey<Enchantment>> disabledEnchantments, Map<Item, Map<RegistryKey<Enchantment>, Integer>> itemEnchantments) {
 
         public EnchantmentsConfig() {
-            this(true, true, true, true, false, Optional.of(Platform.isModLoaded("betterarcheology")), UnruffledMod.DEFAULT_DISABLED_ENCHANTMENTS, UnruffledMod.DEFAULT_ITEM_ENCHANTMENTS);
+            this(
+                true, true, true, true, false,
+                Optional.of(false), Optional.of(true), Optional.of(true), Optional.of(true),
+                UnruffledMod.DEFAULT_DISABLED_ENCHANTMENTS, UnruffledMod.DEFAULT_ITEM_ENCHANTMENTS);
         }
 
         public static Codec<Set<RegistryKey<Enchantment>>> ENCHANTMENT_SET_CODEC = RegistryKey.createCodec(RegistryKeys.ENCHANTMENT).listOf().xmap(HashSet::new, LinkedList::new);
@@ -158,6 +161,9 @@ public class Config {
             Codec.BOOL.fieldOf("disable_glint").forGetter(EnchantmentsConfig::disableGlint),
             Codec.BOOL.fieldOf("display_xp_level_over_bar").forGetter(EnchantmentsConfig::showLevelNumber),
             Codec.BOOL.optionalFieldOf("disenchant_chiseled_bookshelf_books").forGetter(EnchantmentsConfig::disenchantChiseledBookshelfBooks),
+            Codec.BOOL.optionalFieldOf("filter_structure_item_frames").forGetter(EnchantmentsConfig::filterStructureItemFrames),
+            Codec.BOOL.optionalFieldOf("filter_structure_chiseled_bookshelves").forGetter(EnchantmentsConfig::filterStructureChiseledBookshelves),
+            Codec.BOOL.optionalFieldOf("filter_structure_decorated_pots").forGetter(EnchantmentsConfig::filterStructureDecoratedPots),
             ENCHANTMENT_SET_CODEC.fieldOf("disabled_enchantments").forGetter(config -> config.disabledEnchantments),
             Codec.unboundedMap(Registries.ITEM.getCodec(), ENCHANTMENT_MAP_CODEC).fieldOf("intrinsic_enchantments").forGetter(config -> config.itemEnchantments)
             ).apply(instance, EnchantmentsConfig::new)
--- common/src/main/java/io/github/orlouge/unruffled/items/ItemEnchantmentsHelper.java
+++ common/src/main/java/io/github/orlouge/unruffled/items/ItemEnchantmentsHelper.java
@@ -1,20 +1,23 @@
 package io.github.orlouge.unruffled.items;
 
 import io.github.orlouge.unruffled.config.Config;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.BundleContentsComponent;
+import net.minecraft.component.type.ContainerComponent;
 import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.*;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.EnchantmentTags;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import javax.xml.crypto.Data;
+import java.util.*;
 
 public class ItemEnchantmentsHelper {
     public static ItemStack createWithItemEnchantments(Item item, RegistryEntryLookup.RegistryLookup registryLookup) {
@@ -44,5 +47,61 @@ public class ItemEnchantmentsHelper {
 
     public static boolean isDisabled(RegistryKey<Enchantment> enchantment, ItemStack stack) {
         return Config.isLoaded() && Config.INSTANCE.get().enchantmentsConfig.disabledEnchantments().contains(enchantment) && !(Config.INSTANCE.get().enchantmentsConfig.itemEnchantments().getOrDefault(stack.getItem(), Collections.emptyMap()).containsKey(enchantment));
+    }
+
+    public static ItemStack processItem(ItemStack stack, RegistryEntryLookup.RegistryLookup lookup, boolean allowNonRandomLootEnchantments) {
+        stack = stack.copy();
+        boolean hasEnchantments = stack.contains(DataComponentTypes.ENCHANTMENTS);
+        boolean hasStoredEnchantments = stack.contains(DataComponentTypes.STORED_ENCHANTMENTS);
+        while (hasEnchantments || hasStoredEnchantments) {
+            ItemEnchantmentsComponent enchantmentsComponent;
+            if (hasEnchantments) {
+                enchantmentsComponent = stack.get(DataComponentTypes.ENCHANTMENTS);
+            } else {
+                enchantmentsComponent = stack.get(DataComponentTypes.STORED_ENCHANTMENTS);
+            }
+            ItemEnchantmentsComponent.Builder out = new ItemEnchantmentsComponent.Builder(ItemEnchantmentsComponent.DEFAULT);
+            for (Object2IntMap.Entry<RegistryEntry<Enchantment>> entry : enchantmentsComponent.getEnchantmentEntries()) {
+                if (entry.getKey().getKey().isEmpty() || (
+                    !ItemEnchantmentsHelper.isDisabled(entry.getKey().getKey().get(), stack) &&
+                    (allowNonRandomLootEnchantments || entry.getKey().isIn(EnchantmentTags.ON_RANDOM_LOOT))
+                )) {
+                    out.add(entry.getKey(), entry.getValue());
+                }
+            }
+            ItemEnchantmentsComponent outComponent = out.build();
+            if (hasEnchantments) {
+                hasEnchantments = false;
+                stack.set(DataComponentTypes.ENCHANTMENTS, outComponent);
+            } else {
+                hasStoredEnchantments = false;
+                stack.set(DataComponentTypes.STORED_ENCHANTMENTS, outComponent);
+            }
+        }
+
+        if (stack.isOf(Items.ENCHANTED_BOOK) && (!stack.contains(DataComponentTypes.STORED_ENCHANTMENTS) || stack.get(DataComponentTypes.STORED_ENCHANTMENTS).isEmpty())) {
+            stack = stack.copyComponentsToNewStack(Items.BOOK, stack.getCount());
+            if (stack.contains(DataComponentTypes.STORED_ENCHANTMENTS)) stack.remove(DataComponentTypes.STORED_ENCHANTMENTS);
+        }
+
+        if (stack.contains(DataComponentTypes.BUNDLE_CONTENTS)) {
+            BundleContentsComponent bundleComponent = stack.get(DataComponentTypes.BUNDLE_CONTENTS);
+            BundleContentsComponent.Builder out = new BundleContentsComponent.Builder(BundleContentsComponent.DEFAULT);
+            for (ItemStack substack : bundleComponent.iterate()) {
+                out.add(processItem(substack, lookup, allowNonRandomLootEnchantments));
+            }
+            stack.set(DataComponentTypes.BUNDLE_CONTENTS, out.build());
+        }
+
+        if (stack.contains(DataComponentTypes.CONTAINER)) {
+            ContainerComponent bundleComponent = stack.get(DataComponentTypes.CONTAINER);
+            LinkedList<ItemStack> out = new LinkedList<>();
+            for (ItemStack substack : bundleComponent.iterateNonEmpty()) {
+                out.add(processItem(substack, lookup, allowNonRandomLootEnchantments));
+            }
+            stack.set(DataComponentTypes.CONTAINER, ContainerComponent.fromStacks(out));
+        }
+
+        return stack;
     }
 }
